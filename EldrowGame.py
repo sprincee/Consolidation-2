92cae2b7fc442cf3f035cacc06cc76fc057487c1
sprincee <87615605+sprincee@users.noreply.github.com>
2024-05-01 16:14:27 -0400

    Update EldrowGame.py
    Release 1.0
    
    Final Implementations:
    - Added helpful comments throughout code.
    - Removed coding-outline.
    - Added author-tag.

--- EldrowGame.py
+++ EldrowGame.py
@@ -1,44 +1,5 @@
 #Title: Word Guessing Game or 'Eldrow'
 
-'''
-
-Coding-Outline:
-
-
-
-Summary: 
-- A word guessing game; pretty simple.
-
-Object:
-- To guess a secret word from a bank of words.
-
-Parameters:
-- Each turn, the player can guess a letter, or the whole word.
-- At the beginning of the game, there'll be a theme, which can be chosen.
-- After guessing a letter, the game will let the player know how many occurances of that letter are in the word.
-- If the player is right, the letter & position of the letter will show. (Taking inspiration from Wordle)
-- The player only has three 'word' guesses.
-- A player's final score is how many turns it took to find the word.
-
-Notes:
-- Taking inspiration from NYT's Wordle; I'll allow only one player.
-
-Functions/Structural Elements:
-- A list of themes, which can be chosen. --> (Fruits, Animals, Colors)
-- A list of words, which correspond to a theme. --> (TBD)
-- A function to randomly choose words, probably using import random.
-- A function to check if the letter guessed is within the secret word.
-
-
-
-A note for grading:
-- I used this as an initial commit; also as a way to organize my thoughts.
-- It's not all encompassing & things are probably going to change during dev cycle.
-- It's just a way to get my ideas on paper (or screen, in this case).
-
-
-'''
-
 #Imports
 import random
 import time
@@ -83,6 +44,7 @@ def choose_words(theme_choice):
     if theme_choice == 3:
         return random.choice(color_bank)
 
+#A function used return the count of a certain letter within the chosen word
 def check_letter(word, letter):
     return word.count(letter)
 
@@ -97,7 +59,7 @@ def game_begin(theme_choice, chosen_word):
     word_guesses = 0
 
    
-
+    #Implementation of Primary Game-Logic
     while '_' in guessed_word and word_guesses < 3:
         print("Secret Word:", " ".join(guessed_word))
         print("Guessed Letters:", ", ".join(guessed_letters))
@@ -174,4 +136,4 @@ time.sleep(1)
 game_begin(chosen_theme, chosen_word)
 game_v2.credits(username)
 
-
+# PROJECT by MAHAD KHAN from KHAN STUDIOS
